7224a8c60f9e83c0b2d5fbb3680478f4ca3b886f
Fulim13 <87457764+Fulim13@users.noreply.github.com>
2023-08-04 12:05:32 +0800

    Complete tutorial 2

--- Chapter2/src/matrix/SquareMatrix.java
+++ Chapter2/src/matrix/SquareMatrix.java
@@ -1,6 +1,7 @@
 package matrix;
 
 public class SquareMatrix implements SquareMatrixInterface {
+
     private int n;
     private int[][] array;
 
@@ -8,38 +9,64 @@ public class SquareMatrix implements SquareMatrixInterface {
         this.n = n;
         this.array = new int[n][n];
     }
-    
-    
 
     @Override
     public void makeEmpty(int m) {
-//        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-        for (int i = 0; i < m; i++) {
-            
+        if (m >= 1 && m <= n) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < m; j++) {
+                    array[i][j] = 0;
+                }
+            }
         }
     }
 
     @Override
     public void storeValue(int i, int j, int value) {
-        this.array[i-1][j-1] = value;
+        if (i >= 1 && i <= n && j >= 1 && j <= n) {
+            array[i - 1][j - 1] = value;
+        }
     }
-        
 
     @Override
     public SquareMatrixInterface add(SquareMatrixInterface m) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        // downcasting from SquareMatrixInterface to SquareMatrix
+        SquareMatrix givenMatrix = (SquareMatrix) m;
+        if (this.n == givenMatrix.n) {
+            SquareMatrix resultMatrix = new SquareMatrix(n);
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    resultMatrix.array[i][j] = this.array[i][j] + givenMatrix.array[i][j];
+                }
+            }
+            return resultMatrix;
+        }
+        return null;
     }
 
     @Override
     public void copy(SquareMatrixInterface m2) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        SquareMatrix givenMatrix = (SquareMatrix)m2;
+        if (this.n == givenMatrix.n) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    givenMatrix.array[i][j] = this.array[i][j];
+                }
+            }
+        }
+
     }
 
     @Override
     public String toString() {
-        return "SquareMatrix{" + '}';
+        String outputStr = "";
+        for (int i = 0; i < n; i++) { //for each row
+            for (int j = 0; j < n; j++) { // for each column
+                outputStr += String.format("%5d", array[i][j]);
+            }
+            outputStr += "\n";
+        }
+        return outputStr;
     }
-    
-    
 
 }
